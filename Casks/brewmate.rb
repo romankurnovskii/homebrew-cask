2a539ef5c2469b54e9581681b68af9bb27dcbae3
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-17 08:20:36 +0200

    update style

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -2,8 +2,7 @@ cask "brewmate" do
   version "0.2.1"
   sha256 "76776c0d34e3ed97ad7ba334977fc19cfb04af6df9c5a77a1ebc6602771b6649"
 
-  url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal-mac.zip",
-      verified: "github.com/romankurnovskii/BrewMate/"
+  url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal-mac.zip"
   name "BrewMate"
   desc "Homebrew GUI applications manager"
   homepage "https://github.com/romankurnovskii/BrewMate"
