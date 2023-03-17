1d001e5f485bbf75d7e861354bcaf9562b57e08a
Roman Kurnovskii <r.kurnovskii@gmail.com>
2023-03-17 08:14:30 +0200

    refactor style

--- Casks/brewmate.rb
+++ Casks/brewmate.rb
@@ -1,18 +1,15 @@
 cask "brewmate" do
-    version "0.2.1"
-    sha256 "76776c0d34e3ed97ad7ba334977fc19cfb04af6df9c5a77a1ebc6602771b6649"
-  
-    url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal-mac.zip",
-        verified: "github.com/romankurnovskii/BrewMate/"
+  version "0.2.1"
+  sha256 "76776c0d34e3ed97ad7ba334977fc19cfb04af6df9c5a77a1ebc6602771b6649"
 
-    name "BrewMate"
-    desc "Homebrew GUI applications manager"
-    homepage "https://github.com/romankurnovskii/BrewMate"
-  
-    app "BrewMate.app"
-  
-    auto_updates true
-    depends_on macos: ">= :sierra"
-  end
+  url "https://github.com/romankurnovskii/BrewMate/releases/download/#{version}/BrewMate-#{version}-universal-mac.zip",
+      verified: "github.com/romankurnovskii/BrewMate/"
+  name "BrewMate"
+  desc "Homebrew GUI applications manager"
+  homepage "https://github.com/romankurnovskii/BrewMate"
 
-# https://github.com/Homebrew/brew/blob/master/docs/Cask-Cookbook.md
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "BrewMate.app"
+end
